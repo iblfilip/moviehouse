b49f81f8655d63d1478b31834e912575d924b2a0
Filip Ibl <iblfilip@gmail.com>
2017-03-26 23:51:57 +0200

    reading gitkit credetials from properties file

--- src/main/java/com/ibl/moviehouse/tools/GitKitTools.java
+++ src/main/java/com/ibl/moviehouse/tools/GitKitTools.java
@@ -13,18 +13,17 @@ import java.util.logging.Logger;
 public class GitKitTools {
 
     public GitkitUser getGitKitUser(HttpServletRequest request) {
-
+        PropertiesHandler handler = PropertiesHandler.getInstance();
         GitkitUser user = null;
         try {
             GitkitClient gitkitClient = GitkitClient.newBuilder()
-                    .setGoogleClientId("50123732426-86gl9b40apnm8nl2moft5tf7qqhg71sk.apps.googleusercontent.com")
-                    .setProjectId("moviehouse-148209")
-                    .setServiceAccountEmail("")
-                    .setCookieName("gtoken")
-                    .setWidgetUrl("https://moviehouse-148209.appspot.com/gitkit")
-                    .setKeyStream(new ClassPathResource("moviehouse-e9df73d185a0.p12").getInputStream()).build();
+                    .setGoogleClientId(handler.getClientId())
+                    .setProjectId(handler.getProjectId())
+                    .setServiceAccountEmail(handler.getServiceAccountEmail())
+                    .setCookieName(handler.getCookieName())
+                    .setWidgetUrl(handler.getWidgetUrl())
+                    .setKeyStream(new ClassPathResource(handler.getKeyFile()).getInputStream()).build();
 
-            //GitkitClient gitkitClient = GitkitClient.createFromJson("resources/gitkit-server-config.json");
             user = gitkitClient.validateTokenInRequest(request);
 
         } catch (IOException e) {
--- src/main/java/com/ibl/moviehouse/tools/PropertiesHandler.java
+++ src/main/java/com/ibl/moviehouse/tools/PropertiesHandler.java
@@ -7,8 +7,13 @@ import java.util.Properties;
 public class PropertiesHandler {
 
     Properties prop;
-    public static final String PROP_DB_URL = "db_url";
-    public static final String CONFIG_FILE = "config.properties";
+    public static final String PROP_CLIENT_ID = "CLIENT_ID";
+    public static final String PROP_PROJECT_ID= "PROJECT_ID";
+    public static final String PROP_COOKIE_NAME= "COOKIE_NAME";
+    public static final String PROP_SERVICE_ACCOUNT_EMAIL= "SERVICE_ACCOUNT_ID";
+    public static final String PROP_WIDGET_URL= "WIDGET_URL";
+    public static final String PROP_KEY_FILE= "KEY_FILE";
+    public static final String CONFIG_FILE = "gitkit.properties";
 
     private PropertiesHandler() {
         prop = getProperties();
@@ -45,7 +50,10 @@ public class PropertiesHandler {
         return prop;
     }
 
-    public String getDbUrl() {
-        return prop.getProperty(PROP_DB_URL);
-    }
+    public String getClientId() {return prop.getProperty(PROP_CLIENT_ID);}
+    public String getProjectId() {return prop.getProperty(PROP_PROJECT_ID);}
+    public String getCookieName() {return prop.getProperty(PROP_COOKIE_NAME);}
+    public String getServiceAccountEmail() {return prop.getProperty(PROP_SERVICE_ACCOUNT_EMAIL);}
+    public String getWidgetUrl() {return prop.getProperty(PROP_WIDGET_URL);}
+    public String getKeyFile() {return prop.getProperty(PROP_KEY_FILE);}
 }
